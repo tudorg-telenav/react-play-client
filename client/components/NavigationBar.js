6c6c0ebce1c59cfe823b1ba3e15bffeba3389b1e
Tudor Goia <goiadantudor@hotmail.com>
2017-02-13 01:47:05 +0200

    added radium for css

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,35 +1,47 @@
 import React from 'react';
-
-var navStyle = {
-  position: "fixed",
-  height: "100%",
-  width: 60,
-  fontSize: 11,
-  letterSpacing: 2,
-  background: "#fff",
-  overflow: "hidden",
-  top: 0,
-  left: 0,
-  zIndex: 2
-};
-
-// var divStyle = {
-//     position: "relative",
-//     top: 255
-// };
-
-// var ulStyle = {
-//     position: "relative",
-//     width: 1000
-// };
+import Radium from 'radium';
 
 class NavigationBar extends React.Component {
 
+    getStyles() {
+
+        const backgroundColor = '#fff';
+
+        return {
+
+            navStyle: {
+                position: "fixed",
+                height: "100%",
+                width: 60,
+                fontSize: 11,
+                letterSpacing: 2,
+                background: backgroundColor,
+                overflow: "hidden",
+                top: 0,
+                left: 0,
+                zIndex: 2
+            },
+
+            divStyle: {
+                position: "relative",
+                top: 255
+            },
+
+            ulStyle: {
+                position: "relative",
+                width: 1000
+            }
+        };
+    }
+
     render() {
+
+        const styles = this.getStyles();
+
         return (
-            <nav style={navStyle}>
-              <div>
-                <ul>
+            <nav style={styles.navStyle}>
+              <div style={styles.divStyle}>
+                <ul style={styles.ulStyle}>
                 </ul>
               </div>
             </nav>
@@ -37,4 +49,4 @@ class NavigationBar extends React.Component {
     }
 }
 
-export default NavigationBar;
+export default Radium(NavigationBar);
