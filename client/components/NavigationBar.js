bc7797ee4a802fe23026eb12c930e12c5971a638
Tudor Goia <goiadantudor@hotmail.com>
2017-02-13 02:34:37 +0200

    added items in the navbar

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -3,45 +3,69 @@ import Radium from 'radium';
 
 class NavigationBar extends React.Component {
 
+    getItems() {
+        return ['careers', 'press releases'];
+    }
+
     getStyles() {
 
         const backgroundColor = '#fff';
 
         return {
 
-            navStyle: {
-                position: "fixed",
-                height: "100%",
+            nav: {
+                position: 'fixed',
+                height: '100%',
                 width: 60,
                 fontSize: 11,
                 letterSpacing: 2,
                 background: backgroundColor,
-                overflow: "hidden",
+                overflow: 'hidden',
                 top: 0,
                 left: 0,
                 zIndex: 2
             },
 
-            divStyle: {
-                position: "relative",
+            div: {
+                position: 'relative',
                 top: 255
             },
 
-            ulStyle: {
-                position: "relative",
+            ul: {
+                position: 'relative',
                 width: 1000
+            },
+
+            li: {
+
+            },
+
+            a: {
+
             }
+
         };
     }
 
     render() {
 
         const styles = this.getStyles();
+        const items = this.getItems();
+
+        let anchors = [];
+        for (var i = 0; i < items.length; i++) {
+            anchors.push((
+                <li style={styles.li}>
+                    <a style={styles.a} key={i}>{items[i]}</a>
+                </li>
+            ));
+        }
 
         return (
-            <nav style={styles.navStyle}>
-              <div style={styles.divStyle}>
-                <ul style={styles.ulStyle}>
+            <nav style={styles.nav}>
+              <div style={styles.div}>
+                <ul style={styles.ul}>
+                    {anchors}
                 </ul>
               </div>
             </nav>
