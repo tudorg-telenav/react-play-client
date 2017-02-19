729206ed60abcf08567e871d38354360428c1996
Tudor Goia <tudorg@telenav.com>
2017-02-19 20:21:03 +0200

    ListSection became a stateful component to be able to change visibility from within itself (wondering if that's how this should be done)

--- client/careers/ListSection.js
+++ client/careers/ListSection.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import CareersListSectionItem from './ListSectionItem';
 
-const CareersListSection = (props) => {
+class CareersListSection extends React.Component {
 
-  const getStyles = () => {
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      isCollapsed: true
+    };
+  };
+
+  getStyles() {
 
     return {
 
@@ -16,31 +25,74 @@ const CareersListSection = (props) => {
     };
   };
 
-  const styles = getStyles();
-
-  var children = [];
-  if (props.data !== null) {
-    for (var i = 0; i < props.data.length; i++) {
-      children.push(
-        <CareersListSectionItem
-          isSelected={props.selectedCareerId === props.data[i].id}
-          onSelect={props.onSectionIntemSelection}
-          title={props.data[i].title}
-          key={i}
-          id={props.data[i].id}
-        />
-      );
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.selectedCareerId !== null) {
+      if (this.containsItem(nextProps.selectedCareerId)) {
+        this.setState({
+          isCollapsed: false
+        });
+      } else {
+        this.setState({
+          isCollapsed: true
+        });
+      }
     }
   }
 
-  return (
-    <div
-      style={styles.div}
-    >
-      <span>{props.section}</span>
-      {children}
-    </div>
-  );
+  containsItem(itemId) {
+
+    for (var i = 0; i < this.props.data.length; i++) {
+      if (itemId === this.props.data[i].id) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  handleClick() {
+    this.setState({
+      isCollapsed: !this.state.isCollapsed
+    });
+  };
+
+  render() {
+
+    var children = [];
+
+    if (!this.state.isCollapsed) {
+      if (this.props.data !== null) {
+        for (var i = 0; i < this.props.data.length; i++) {
+          children.push(
+            <CareersListSectionItem
+              isSelected={this.props.selectedCareerId === this.props.data[i].id}
+              onSelect={this.props.onSectionIntemSelection}
+              title={this.props.data[i].title}
+              key={i}
+              id={this.props.data[i].id}
+            />
+          );
+        }
+      }
+    }
+
+    const styles = this.getStyles();
+
+    return (
+      <div
+        style={styles.div}
+      >
+        <span>
+          <strong>
+            {this.props.section}
+          </strong>
+          <button onClick={this.handleClick.bind(this)}>
+            {this.state.isCollapsed ? '+' : '-'}
+          </button>
+        </span>
+        {children}
+      </div>
+    );
+  }
 
 };
 
