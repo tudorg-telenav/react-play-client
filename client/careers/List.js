b419ef504a93f0ed78788506fcd5382794cc2c60
Tudor Goia <tudorg@telenav.com>
2017-02-20 07:43:11 +0200

    fixed accordion

--- client/careers/List.js
+++ client/careers/List.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import CareersListSection from './ListSection';
 
-const CareersList = (props) => {
+class CareersList extends React.Component {
 
-  const getStyles = () => {
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      section: null
+    };
+  };
+
+  getStyles() {
 
     return {
 
@@ -15,36 +24,77 @@ const CareersList = (props) => {
       }
 
     };
-  };
+  }
 
-  const styles = getStyles();
+  componentWillReceiveProps(nextProps) {
 
-  var children = [];
-  var i = 0;
-  for (var section in props.data) {
-    if (props.data.hasOwnProperty(section)) {
-      children.push(
-        <CareersListSection
-          selectedCareerId={props.selectedCareerId}
-          onSectionIntemSelection={props.onListItemSelection}
-          section={section}
-          data={props.data[section]}
-          key={i}
-        />
-      );
-      i++;
+    if (nextProps.data !== null && nextProps.selectedCareerId !== null) {
+      for (var section in nextProps.data) {
+        if (nextProps.data.hasOwnProperty(section)) {
+
+          if (this.sectionContainsItem(section, nextProps.selectedCareerId)) {
+            this.setState({
+              section
+            });
+          }
+        }
+      }
     }
   }
 
-  return (
+  toggleSection(section) {
+    if (this.state.section !== section) {
+      this.setState({
+        section
+      });
+    }
+  }
 
-    <div
-      style={styles.div}
-    >
-      <h2>Careers List</h2>
-      {children}
-    </div>
-  );
+  sectionContainsItem(section, itemId) {
+
+    var data = this.props.data[section];
+
+    for (var i = 0; i < data.length; i++) {
+      if (itemId === data[i].id) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  render() {
+
+    var styles = this.getStyles();
+
+    var children = [];
+    var i = 0;
+    for (var section in this.props.data) {
+      if (this.props.data.hasOwnProperty(section)) {
+        children.push(
+          <CareersListSection
+            onSectionToggled={this.toggleSection.bind(this)}
+            isCollapsed={this.state.section !== section}
+            selectedCareerId={this.props.selectedCareerId}
+            onSectionIntemSelection={this.props.onListItemSelection}
+            section={section}
+            data={this.props.data[section]}
+            key={i}
+          />
+        );
+        i++;
+      }
+    }
+
+    return (
+
+      <div
+        style={styles.div}
+      >
+        <h2>Careers List</h2>
+        {children}
+      </div>
+    );
+  }
 
 };
 
--- client/careers/ListSection.js
+++ client/careers/ListSection.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import CareersListSectionItem from './ListSectionItem';
 
-class CareersListSection extends React.Component {
+const CareersListSection = (props) => {
 
-  constructor(props) {
-
-    super(props);
-
-    this.state = {
-      isCollapsed: true
-    };
-  };
-
-  getStyles() {
+  const getStyles = () => {
 
     return {
 
@@ -25,74 +16,45 @@ class CareersListSection extends React.Component {
     };
   };
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.selectedCareerId !== null) {
-      if (this.containsItem(nextProps.selectedCareerId)) {
-        this.setState({
-          isCollapsed: false
-        });
-      } else {
-        this.setState({
-          isCollapsed: true
-        });
+  const handleClick = () => {
+    props.onSectionToggled(props.section);
+  };
+
+  var children = [];
+
+  if (!props.isCollapsed) {
+    if (props.data !== null) {
+      for (var i = 0; i < props.data.length; i++) {
+        children.push(
+          <CareersListSectionItem
+            isSelected={props.selectedCareerId === props.data[i].id}
+            onSelect={props.onSectionIntemSelection}
+            title={props.data[i].title}
+            key={i}
+            id={props.data[i].id}
+          />
+        );
       }
     }
   }
 
-  containsItem(itemId) {
+  const styles = getStyles();
 
-    for (var i = 0; i < this.props.data.length; i++) {
-      if (itemId === this.props.data[i].id) {
-        return true;
-      }
-    }
-    return false;
-  };
-
-  handleClick() {
-    this.setState({
-      isCollapsed: !this.state.isCollapsed
-    });
-  };
-
-  render() {
-
-    var children = [];
-
-    if (!this.state.isCollapsed) {
-      if (this.props.data !== null) {
-        for (var i = 0; i < this.props.data.length; i++) {
-          children.push(
-            <CareersListSectionItem
-              isSelected={this.props.selectedCareerId === this.props.data[i].id}
-              onSelect={this.props.onSectionIntemSelection}
-              title={this.props.data[i].title}
-              key={i}
-              id={this.props.data[i].id}
-            />
-          );
-        }
-      }
-    }
-
-    const styles = this.getStyles();
-
-    return (
-      <div
-        style={styles.div}
-      >
-        <span>
-          <strong>
-            {this.props.section}
-          </strong>
-          <button onClick={this.handleClick.bind(this)}>
-            {this.state.isCollapsed ? '+' : '-'}
-          </button>
-        </span>
-        {children}
-      </div>
-    );
-  }
+  return (
+    <div
+      style={styles.div}
+    >
+      <span>
+        <button onClick={handleClick.bind(this)}>
+          {props.isCollapsed ? '+' : '-'}
+        </button>
+        <strong>
+          {props.section}
+        </strong>
+      </span>
+      {children}
+    </div>
+  );
 
 };
 
