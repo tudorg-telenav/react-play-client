715d96fdd1f1c1cea44b231fe16ab7f739438b74
Tudor Goia <tudorg@telenav.com>
2017-02-21 15:14:46 +0200

    removed redundant requires

--- webpack.client.dev.config.js
+++ webpack.client.dev.config.js
@@ -1,5 +1,5 @@
-var path = require('path');
-var webpack = require('webpack');
+// var path = require('path');
+// var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var APP_ROOT = 'client';
